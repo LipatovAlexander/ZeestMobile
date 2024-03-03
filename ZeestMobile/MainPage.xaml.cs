51cf779d570f21ddf22be6f29ccbafaf2518f6c2
Alexander Lipatov <lipatov@mindbox.cloud>
2024-03-03 18:06:26 +0300

    feat: Add new todo list

--- ZeestMobile/MainPage.xaml.cs
+++ ZeestMobile/MainPage.xaml.cs
@@ -4,9 +4,19 @@ namespace ZeestMobile;
 
 public partial class MainPage : ContentPage
 {
+    private readonly TodoListsViewModel _todoListsViewModel;
+    
     public MainPage(TodoListsViewModel todoListsViewModel)
     {
         InitializeComponent();
         BindingContext = todoListsViewModel;
+        _todoListsViewModel = todoListsViewModel;
+    }
+
+    private void TodoList_OnTextChanged(object? sender, TextChangedEventArgs e)
+    {
+        var text = e.NewTextValue;
+
+        _todoListsViewModel.NewListName = text;
     }
 }
--- ZeestMobile/ViewModels/TodoListsViewModel.cs
+++ ZeestMobile/ViewModels/TodoListsViewModel.cs
@@ -1,29 +1,67 @@
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using ZeestMobile.Infrastructure.EntityFramework;
 using ZeestMobile.Model;
 
 namespace ZeestMobile.ViewModels;
 
-public class TodoListsViewModel(ApplicationContext applicationContext) : INotifyPropertyChanged
+public class TodoListsViewModel : INotifyPropertyChanged
 {
-    public IReadOnlyList<TodoList> TodoLists => applicationContext.TodoLists.ToList();
+    private readonly ApplicationContext _applicationContext;
+
+    public ObservableCollection<TodoList> TodoLists { get; set; }
+
+    public TodoListsViewModel(ApplicationContext applicationContext)
+    {
+        _applicationContext = applicationContext;
+        TodoLists = new ObservableCollection<TodoList>(_applicationContext.TodoLists.ToList());
+
+        AddItemCommand = new Command(AddTodoList);
+    }
+
+    private string _newListName = null!;
+    public string NewListName
+    {
+        get => _newListName;
+        set
+        {
+            if (value != _newListName)
+            {
+                _newListName = value;
+                OnPropertyChanged();                
+            }
+        }
+    }
+
+    public ICommand AddItemCommand { get; }
     
+    private void AddTodoList()
+    {
+        if (string.IsNullOrWhiteSpace(NewListName))
+        {
+            return;
+        }
+        
+        var todoList = new TodoList(NewListName)
+        {
+            Items = []
+        };
+
+        _applicationContext.TodoLists.Add(todoList);
+        _applicationContext.SaveChanges();
+        
+        TodoLists.Add(todoList);
+        OnPropertyChanged(nameof(TodoLists));
+
+        NewListName = null!;
+    }
+
     public event PropertyChangedEventHandler? PropertyChanged;
 
     protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    public void AddTodoList(string name)
-    {
-        var todoList = new TodoList(name)
-        {
-            Items = []
-        };
-
-        applicationContext.TodoLists.Add(todoList);
-        applicationContext.SaveChanges();
     }
 }
